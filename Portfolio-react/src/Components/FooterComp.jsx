b84a1a371d4407d5096d478372af7167e38152be
RRTRACER <156194883+RRTracer@users.noreply.github.com>
2025-02-26 14:11:48 +0100

    test push svg qui veulent pas passer

--- Portfolio-react/src/Components/FooterComp.jsx
+++ Portfolio-react/src/Components/FooterComp.jsx
@@ -5,7 +5,7 @@ const FooterComp = () => {
     <div className="dark:text-white flex flex-row items-center justify-between p-10  shadow-2xl w-full h-2 absolute ">
       <a href="#"><img className="w-[5rem] h-auto" src="Img/logo.png" alt=""/></a>
       <a href="#"><h1 className="text-xl font-bold"> Thanks to have read my React&apos;s portfolio !</h1></a>
-      <ul className="flex flex-col italic dark:list-image-[url(Img/map-pin.svg)] list-image-[url(Img/map-pin-lightmode.svg)] p-2 gap-2">
+      <ul className="flex flex-col italic dark:list-image-[url(Img/map-pin2.svg)] list-image-[url(Img/map-pin-lightmode2.svg)] p-2 gap-2">
         <li><a href="https://github.com/RRTracer">My Github</a></li>
         <li><a className="flex flex-row" href="https://framagit.org/rrtracer">My Framagit ( empty&nbsp;&nbsp;<div className="dark:text-yellow-300"><Laugh /></div>&nbsp;&nbsp;)</a></li>
       </ul>
