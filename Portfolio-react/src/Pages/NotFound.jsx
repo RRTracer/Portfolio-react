e442890386fa6e64812aa970f4e7247f52765eea
RRTRACER <156194883+RRTracer@users.noreply.github.com>
2025-03-06 23:17:33 +0100

    Delete Portfolio-react directory

--- Portfolio-react/src/Pages/NotFound.jsx
+++
@@ -1,8 +0,0 @@
-const NotFound = () => {
-   return (
-     <div className="flex justify-center items-center h-screen">
-       <h1 className="text-7xl dark:text-white text-black ">404 , page not found ...</h1>
-     </div>
-   )
-}
-export default NotFound;
